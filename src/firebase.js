c629460bdfa622fb924a3a95abb4a43ccbe4d742
dam1007 <dam1007@naver.com>
2022-12-08 23:53:33 +0900

    1.1 Initialization

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAV6wY9rPs8VW2PlJxMhffHHb5jA4CZAhI",
+    authDomain: "nwitter-8d76b.firebaseapp.com",
+    projectId: "nwitter-8d76b",
+    storageBucket: "nwitter-8d76b.appspot.com",
+    messagingSenderId: "499123278349",
+    appId: "1:499123278349:web:60e2423dce0c607d1bfb44"
+};
+
+export default firebase.initializeApp(firebaseConfig);
